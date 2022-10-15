dabbe57f7eee60ed2049b36c7d455d35c4ac30ef
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-10-15 09:56:20 +0200

    LEanring swift functions and tubples

---
+++ FunctionsSwift.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+     
+}
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+
+func printWithoutCounting(string: String) {
+    let _ = printAndCount(string: string)
+}
+
+func printWithoutCountingDeeper(string: String) {
+   printAndCount(string: string)
+}
+
+func runApp() -> () {
+    // () represents void
+    print(greet(person: "Karabo"))
+    print(greet(person: "Nikita", alreadyGreeted: true))
+    print(printAndCount(string: "Fikasentani"))
+    print(printWithoutCounting(string: "Thulani"))
+    print(printWithoutCountingDeeper(string: "Mpilo"))
+    
+    
+}
+
+// Multiple Return Values and Tuples
+let myTuple = (me: "Mpilo", myLady: "Thapelo")
+
+print(myTuple.me)
+print(myTuple.myLady)
+print(myTuple.0)
+print(myTuple.1)
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+
+let bounds = minMax(array: [18, -40, 100, 20, 10, 77, 87, 35])
+print("min is \(bounds.min) and max is \(bounds.max)")
+
+
+
+
+
+
+
+
+
+runApp()
+
+
+
