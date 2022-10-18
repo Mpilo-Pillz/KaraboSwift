10ca314ce0d5f78629dba469188d0b972b81fc95
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-10-18 19:14:45 +0200

    more on arrays

--- SwiftArrays.playground/Contents.swift
+++ SwiftArrays.playground/Contents.swift
@@ -2,3 +2,16 @@ import UIKit
 
 var someInts: [Int] = []
 print("someInts is of type [Int] with \(someInts.count) items.")
+someInts.append(3)
+someInts = []
+
+var threeDoubles = Array(repeating: 0.0, count: 3)
+
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+
+var sixDoubles = threeDoubles + anotherThreeDoubles
+
+// array literal
+var shoppingList: [String] = ["Eggs", "Milk"]
+// implicit
+var shoppingList = ["Eggs", "Milk"]
