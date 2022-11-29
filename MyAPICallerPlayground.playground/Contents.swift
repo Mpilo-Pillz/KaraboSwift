bafe0a817d211fcc91398fe3150415313ebf566d
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-29 13:12:45 +0200

    adding codable response

--- MyAPICallerPlayground.playground/Contents.swift
+++ MyAPICallerPlayground.playground/Contents.swift
@@ -2,6 +2,11 @@ import UIKit
 
 var greeting = "Hello, playground"
 
+struct Response: Codable {
+let isAuthenticated: Bool
+let message: String
+}
+
 func makePostRequest() {
     guard let url = URL(string: "http://localhost:4000/api/v1/auth/login") else {
         return
@@ -34,4 +39,37 @@ func makePostRequest() {
     task.resume()
 }
 
-makePostRequest()
+func makeCodablePostRequest() {
+    guard let url = URL(string: "http://localhost:4000/api/v1/auth/login") else {
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    // method, body, headers
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "email": "invalid@login.co.za",
+    "password": "notAuth"
+    ]
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    // Make request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONDecoder().decode(Response.self, from: data)
+            print("success: \(response)")
+        } catch {
+            print(error)
+        }
+    }
+    task.resume()
+}
+
+//makePostRequest()
+makeCodablePostRequest()
