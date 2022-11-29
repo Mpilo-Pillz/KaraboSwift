17aa65cee3de3073154814ad2f0312807d230ff8
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-11-29 12:12:45 +0200

    leanring to make a post request with swift

---
+++ MyAPICallerPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+func makePostRequest() {
+    guard let url = URL(string: "http://localhost:4000/api/v1/auth/login") else {
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    // method, body, headers
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let body: [String: AnyHashable] = [
+        "email": "thulani@karabo.co.za",
+    "password": "Thapelo"
+    ]
+    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    // Make request
+    let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        guard let data = data, error == nil else {
+            return
+        }
+        
+        do {
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print("success: \(response)")
+        } catch {
+            print(error)
+        }
+    }
+    task.resume()
+}
+
+makePostRequest()
