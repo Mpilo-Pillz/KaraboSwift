f56a58a22fa20be3d5fe173577b1ef12eaa8601e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-10 13:25:56 +0200

    mapping numbers to words numbers

--- SwiftClosures.playground/Contents.swift
+++ SwiftClosures.playground/Contents.swift
@@ -26,3 +26,35 @@ var ridiculouslyShorterForwardedNames = names.sorted(by: >)
 
 //081180
 // i need a reference number
+
+// Trailing Closures
+var trailingForwardedNames = names.sorted() { $0 < $1 }
+var traiilingForwardedNamesClosureThatDowNotNEedToBeCalled = names.sorted { $0 > $1 }
+
+
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+
+let numbers = [16, 58, 510]
+
+print(16 % 10) // 6
+print(58 % 10) // 8
+print(510 % 10) // 0
+
+let strings = numbers.map { (number) -> String in
+    print("--->\(number)")
+    var number = number
+    var output = ""
+    repeat {
+        print("repeatedNumber--->\(number)")
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    return output
+}
+
+print(strings)
+
+var num = 1 / 10
