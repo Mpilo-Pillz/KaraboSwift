a5a000cd74e4d147cbf928f2fc76c513f8ad0ea0
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-12 11:34:49 +0200

    leanring enum raw values

--- SwiftEnums.playground/Contents.swift
+++ SwiftEnums.playground/Contents.swift
@@ -42,3 +42,101 @@ default:
     print("Not a safe place for humans")
 }
 // Prints "Mostly harmless"
+
+/**
+ Itertable enums
+ */
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
+}
+let numberOfChoices = Beverage.allCases.count
+print("\(numberOfChoices) beverages available")
+// Prints "3 beverages available"
+
+for beverage in Beverage.allCases {
+    print(beverage)
+}
+// coffee
+// tea
+// juice
+
+/**
+ You can define Swift enumerations to store associated values of any given type, and the value types can be different for each case of the enumeration if needed. Enumerations similar to these are known as discriminated unions, tagged unions, or variants in other programming languages.
+ */
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
+
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, let product, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .qrCode(let productCode):
+    print("QR code: \(productCode).")
+}
+// Prints "QR code: ABCDEFGHIJKLMNOP."
+
+switch productBarcode {
+case let .upc(numberSystem, manufacturer, product, check):
+    print("UPC : \(numberSystem), \(manufacturer), \(product), \(check).")
+case let .qrCode(productCode):
+    print("QR code: \(productCode).")
+}
+// Prints "QR code: ABCDEFGHIJKLMNOP."
+
+/**
+ Raw Values
+
+ The barcode example in Associated Values shows how cases of an enumeration can declare that they store associated values of different types. As an alternative to associated values, enumeration cases can come prepopulated with default values (called raw values), which are all of the same type.
+ */
+
+enum ASCIIControlCharacter: Character {
+    case tab = "\t"
+    case lineFeed = "\n"
+    case carriageReturn = "\r"
+}
+
+/**
+ Implicitly Assigned Raw Values
+ When you’re working with enumerations that store integer or string raw values, you don’t have to explicitly assign a raw value for each case. When you don’t, Swift automatically assigns the values for you.
+
+ For example, when integers are used for raw values, the implicit value for each case is one more than the previous case. If the first case doesn’t have a value set, its value is 0.
+
+
+ */
+
+enum PlanetImplicit: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+enum CompassPointImplicit: String {
+    case north, south, east, west
+}
+
+enum CompassPointExplicit: String {
+    case north = "NORTH", south = "SOUTH", east = "EAST", west = "WEST"
+}
+
+
+/** You access the raw value of an enumeration case with its rawValue property: */
+let earthsOrder = PlanetImplicit.earth.rawValue
+// earthsOrder is 3
+
+let sunsetDirection = CompassPointImplicit.west.rawValue
+// sunsetDirection is "west"
+
+print(
+    ASCIIControlCharacter.tab
+)
+print(ASCIIControlCharacter.lineFeed.rawValue)
+print(ASCIIControlCharacter.carriageReturn.rawValue)
+
+print(CompassPointExplicit.west.rawValue)
+print(CompassPointExplicit.north.rawValue)
+print(CompassPointExplicit.south.rawValue)
+print(CompassPointExplicit.east.rawValue)
+
