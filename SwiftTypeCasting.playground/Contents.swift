1184ae11c77628857faf685728f583499d419ce5
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-15 22:38:02 +0200

    completing type casting chapter

--- SwiftTypeCasting.playground/Contents.swift
+++ SwiftTypeCasting.playground/Contents.swift
@@ -70,3 +70,54 @@ for item in library {
         print("Song: \(song.name), by \(song.artist)")
     }
 }
+
+/**
+ Type Casting for Any and AnyObject
+
+ Swift provides two special types for working with nonspecific types:
+
+ Any can represent an instance of any type at all, including function types.
+
+ AnyObject can represent an instance of any class type.
+ */
+
+var things: [Any] = []
+
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Song(name: "Mlazi Milano", artist: "Okmalumkoolkat"))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+for thing in things {
+    switch thing {
+    case 0 as Int:
+        print("zero as an Int")
+    case 0 as Double:
+        print("zero as a Double")
+    case let someInt as Int:
+        print("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        print("a positive double value of \(someDouble)")
+    case is Double:
+        print("some other double value that I don't want to print")
+    case let someString as String:
+        print("a string value of \"\(someString)\"")
+    case let (x, y) as (Double, Double):
+        print("an (x, y) point at \(x), \(y)")
+    case let movie as Movie:
+        print("a movie called \(movie.name), dir. \(movie.director)")
+    case let stringConverter as (String) -> String:
+        print(stringConverter("Michael"))
+    default:
+        print("something else")
+    }
+}
+
+let optionalNumber: Int? = 3
+things.append(optionalNumber)        // Warning
+things.append(optionalNumber as Any) // No warning
+
