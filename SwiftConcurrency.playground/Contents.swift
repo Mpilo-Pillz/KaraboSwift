7cd87a74a4ecef866bf92a070a202b0b125d7b9d
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-17 14:30:45 +0200

    using task dot sleep to simulate a network call

--- SwiftConcurrency.playground/Contents.swift
+++ SwiftConcurrency.playground/Contents.swift
@@ -24,3 +24,8 @@ let sortedNames = photoNames.sorted()
 let name = sortedNames[0]
 let photo = await downloadPhoto(named: name)
 show(photo: photo)
+
+func listPhotos(inGallery name: String) async throws -> [String] {
+    try await Task.sleep(until: .now + .seconds(2), clock: .continuous)
+    return ["IMG001", "IMG99", "IMG0404"]
+}
