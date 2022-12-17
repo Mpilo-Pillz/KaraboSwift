1aa2ce664a9c89b45dac8564c6367d4ad4b4d15b
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-17 14:12:45 +0200

    learning concurrency

---
+++ SwiftConcurrency.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+let photoNames: [String] = ["Beach", "Mountains", "Ocean", "Sea", "Landscape"]
+
+func show(photo: String) {
+    print(photoNames)
+}
+
+//listPhotos(inGallery: "Summer Vacation") { photoNames in
+//    let sortedNames = photoNames.sorted()
+//    let name = sortedNames[0]
+//    downloadPhoto(named: name) { photo in
+//        show(photo)
+//    }
+//}
+
+func listPhotos(inGallery name: String) async -> [String] {
+    let result = ["some value that comes form a newtwork call"];
+    return result
+}
+
+let photoNames = await listPhotos(inGallery: "Summer Vacation")
+let sortedNames = photoNames.sorted()
+let name = sortedNames[0]
+let photo = await downloadPhoto(named: name)
+show(photo: photo)
