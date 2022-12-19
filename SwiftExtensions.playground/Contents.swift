a4e21ed7835087195636534c7c571c626b49eedd
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-19 18:18:48 +0200

    creating a struct that takes in others structs

--- SwiftExtensions.playground/Contents.swift
+++ SwiftExtensions.playground/Contents.swift
@@ -21,3 +21,20 @@ print("10 km is \(tenKiloMeter) meters")
 
 let aMarathon = 42.km + 195.m
 print("A marathon is \(aMarathon) meters long")
+
+
+/** Initializers */
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
