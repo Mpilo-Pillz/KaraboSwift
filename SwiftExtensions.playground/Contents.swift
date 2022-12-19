7bb5f93b2a366f30da30170a1a5507d52c81725b
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-19 18:32:39 +0200

    looking at the extension

--- SwiftExtensions.playground/Contents.swift
+++ SwiftExtensions.playground/Contents.swift
@@ -38,3 +38,15 @@ struct Rect {
 
 let defaultRect = Rect()
 let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+// centerRect's origin is (2.5, 2.5) and its size is (3.0, 3.0)
+
