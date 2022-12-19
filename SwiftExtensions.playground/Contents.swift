faa26b50c943a9373fd64664d239bb68bbc907fe
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-19 19:18:12 +0200

    learning mutating

--- SwiftExtensions.playground/Contents.swift
+++ SwiftExtensions.playground/Contents.swift
@@ -64,3 +64,12 @@ extension Int {
     print("Hello!")
 }
 
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square()
+// someInt is now 9
+
