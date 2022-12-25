0e251b1529dac534ee916ab97c85fe7168e2f8c4
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-25 23:39:40 +0200

    addin more steps to obsrever

--- SwiftProperties.playground/Contents.swift
+++ SwiftProperties.playground/Contents.swift
@@ -120,4 +120,7 @@ let stepCounter = StepCounter()
 stepCounter.totalSteps = 200
 // About to set totalSteps to 200
 // Added 200 steps
+stepCounter.totalSteps = 360
+// About to set totalSteps to 360
+// Added 160 steps
 
