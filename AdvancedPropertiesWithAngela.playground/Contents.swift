e8fac223e92f4b50d4b50f24831dbb3997be7e2c
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-26 13:10:00 +0200

    leanrign computed properties

---
+++ AdvancedPropertiesWithAngela.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let pizzaInInches: Int = 10
+
+var numberOfSlices: Int = 6
+
+// Cannot infer data type
+// has to be a variable not a constant
+var numberOfSlicesComputedGetter: Int {
+    return pizzaInInches - 4
+}
+
+
+
+let a = numberOfSlicesComputedGetter * 2
+
+print(numberOfSlices)
