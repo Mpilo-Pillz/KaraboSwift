f40b4359296993eec85274c6ff4d6eb2c0eebc8e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-28 08:44:14 +0200

    learning getters

--- AdvancedPropertiesWithAngela.playground/Contents.swift
+++ AdvancedPropertiesWithAngela.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 let pizzaInInches: Int = 10
 
-var numberOfSlices: Int = 6
+var numberOfSlicesStored: Int = 6
 
 // Cannot infer data type
 // has to be a variable not a constant
@@ -10,8 +10,16 @@ var numberOfSlicesComputedGetter: Int {
     return pizzaInInches - 4
 }
 
+// if your find yourself creating methods that do not have an input and an output
+// potentially use a computed property
 func ifComputedPropertiesWerenotAvailabe() -> Int {
     return pizzaInInches - 4
+}
+
+var numberOfSlices: Int {
+    get {
+        return pizzaInInches - 4
+    }
 }
 
 let a = numberOfSlicesComputedGetter * 2
