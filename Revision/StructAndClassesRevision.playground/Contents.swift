22458cc63d2ed1440ad61a675fe487f081b599e2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-29 13:30:00 +0200

    creating struct and class

--- Revision/StructAndClassesRevision.playground/Contents.swift
+++ Revision/StructAndClassesRevision.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+class Laptop {
+    let name: String
+    let model: String
+    let storage: Int
+    
+    init(name: String, model: String, storage: Int) {
+        self.name = name
+        self.storage = storage
+        self.model = model
+    }
+}
+
+struct Phone {
+    var name:  String
+    var model: String
+    var storage: String
+    
+    init(name: String, model: String, storage: String) {
+        self.name = name
+        self.storage = storage
+        self.model = model
+    }
+}
+
+var sisekosLaptop = Laptop(name: "Apple Intel", model: "Mac Book Pro 16", storage: 512)
+var mpilosLaptop = Laptop(name: "Apple Intel", model: "Mac Book Pro 13", storage: 256)
+var BandilesLAptop = Laptop(name: "Apple Silicone", model: "Mac Book Pro 14", storage: 512)
+
+var athisLaptop = sisekosLaptop
+
+
