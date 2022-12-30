89f5bb7047159201b21d139e75dbc32337d0e902
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-30 10:37:25 +0200

    completing challenge

--- challenges/ChallengePaintCalculator.playground/Contents.swift
+++ challenges/ChallengePaintCalculator.playground/Contents.swift
@@ -5,18 +5,26 @@ import UIKit
 
 var width: Float = 1.5
 var height: Float = 2.3
-
+let areaCoveredPerBucket: Float = 1.5
 
 var bucketsOfPaint: Int  {
     get {
         let area = height * width
-        let areaCoveredPerBucket: Float = 1.5
+        
         let numberOfBuckets = area / areaCoveredPerBucket
         let roundedBuckets = ceil(numberOfBuckets)
         return  Int(roundedBuckets)
+    }
+    
+    set {
+
+        let amountThatCanBeCovered = Double(newValue) * Double(areaCoveredPerBucket)
+        print("We have \(newValue) buckets")
+        print("This amount of paint can cover \(amountThatCanBeCovered)")
     }
     
 }
 
 print("you will need \(bucketsOfPaint) buckets")
 
+bucketsOfPaint = 8
