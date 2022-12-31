4cba5418faa33d28475ceb3413db5bf764491c60
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-12-31 21:02:03 +0200

    leanring protocol syntax

---
+++ SwiftProtocols.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+/**
+ A protocol defines a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol.
+
+ In addition to specifying requirements that conforming types must implement, you can extend a protocol to implement some of these requirements or to implement additional functionality that conforming types can take advantage of.
+ */
+
+/*
+If a class has a superclass, list the superclass name before any protocols it adopts, followed by a comma:
+*/
+
+//class SomeClass: SomeSuperclass, FirstProtocol, AnotherProtocol {
+//    // class definition goes here
+//}
