f766bc00ea0164e81845003489c884db1ce4a8fb
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-03-16 12:12:45 +0200

    learninginhetitense and init

--- DesignPatterns/Solid/InterfaceSlashProtocolSegregation.playground/Contents.swift
+++ DesignPatterns/Solid/InterfaceSlashProtocolSegregation.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class Vehicle {
+    func drive() -> Void {
+        print("Vrooommmmm")
+    }
+    
+    func honk() -> Void {
+        print("beep")
+    }
+}
+
+class Car: Vehicle {
+    let brand: String
+    
+     init(brand: String) {
+        self.brand = brand
+        super.init()
+    }
+}
+
+let vehicle = Vehicle();
+vehicle.drive()
+vehicle.honk()
+
+let lexusSedan = Car(brand: "Lexus LS")
+lexusSedan.drive();
+lexusSedan.honk()
+
+
