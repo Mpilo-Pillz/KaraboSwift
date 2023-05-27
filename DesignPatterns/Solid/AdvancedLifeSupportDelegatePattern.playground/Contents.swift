89e0674dbecf07acb774e59dcd353eba48022974
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-27 18:59:25 +0200

    revising delegates

---
+++ DesignPatterns/Solid/AdvancedLifeSupportDelegatePattern.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me whats going on")
+    }
+    
+    func takeCall() {
+        print("anser emergency call")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+struct Paramedic: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second")
+    }
+    
+    func useStethescope() {
+        print("Listening for heaet sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sing staying alive while perform compressions")
+    }
+    
+    func useElectricDrill() {
+        print("Drilling")
+    }
+}
+
+let emihle = EmergencyCallHandler()
+//let phetsile = Paramedic(handler: emihle)
+//let sanele = Surgeon(handler: emihle)
+let dumisani = Doctor(handler: emihle)
+
+emihle.assessSituation()
+emihle.medicalEmergency()
+
+let sae = iOSDriver()
+
+mobiquity.sendkey(filed, value)
