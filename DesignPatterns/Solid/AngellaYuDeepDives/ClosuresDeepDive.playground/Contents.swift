865137964a616402292403ffc53865c8685f8bd4
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-08-12 11:28:21 +0200

    learning to use array helpers with closures

--- DesignPatterns/Solid/AngellaYuDeepDives/ClosuresDeepDive.playground/Contents.swift
+++ DesignPatterns/Solid/AngellaYuDeepDives/ClosuresDeepDive.playground/Contents.swift
@@ -25,3 +25,33 @@ print(result)
 let ifTheLastParameterIsAClosure  = calculator(n1: 1, n2: 0) {$0 + $1}
 print(ifTheLastParameterIsAClosure) // aka trailing closure
 
+// Real world example
+
+let array = [1,0,9,2,8,3,4,8,7,5,6]
+
+func addOne(n1: Int) -> Int {
+    return n1 + 1
+}
+
+array.map(addOne)
+
+array.map({(n1: Int) -> Int in
+    return n1 - 2
+})
+
+array.map({$0 * 3})
+print(
+    array.map{$0 * 4}
+)
+
+let newArray = array.map{"\($0)"}
+print(newArray)
+
+let numArr = [10,29,38,47,56]
+
+print(
+    numArr.filter({$0 < 40})
+)
+print(numArr.filter{$0 > 40})
+
+print(numArr.reduce(0) {$0 + $1})
