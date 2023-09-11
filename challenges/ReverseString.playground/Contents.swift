849f6305928c392f198d1a4e0acbdd9e47d0db1d
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-09-11 23:11:46 +0200

    starting challenge to reverse strings

---
+++ challenges/ReverseString.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+func reverseString(str: String) {
+    // create array to be revered
+let stringAsArray = Array(str)
+    print(stringAsArray)
+    // create empty array to put in reversed strings
+let emptyArrayToBeString = [String]()
+    // loop from the back of the array and add to empty array
+    // convert array to tring
+}
+
+reverseString(str: "Mpilo")
