a6d5e677054a37f322f51502265e9ea05b1a17a8
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-11-05 15:41:52 +0200

    deteriminging how much we can paint

--- DesignPatterns/Solid/AngellaYuDeepDives/ComputedBuckestOfPaint.playground/Contents.swift
+++ DesignPatterns/Solid/AngellaYuDeepDives/ComputedBuckestOfPaint.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var width: Float = 1.5
+var height: Float = 2.3
+
+var bucketsOfPaint: Int {
+    get {
+//        return (width * height) / 1.5
+        let area = width * height
+        let areaCoveredPerBucket: Float = 1.5
+        let numberOfBuckets = area / areaCoveredPerBucket
+        let roundedBuckets = ceil(numberOfBuckets)
+        return Int(roundedBuckets)
+    }
+    set {
+        let areaCanCover = Double(newValue) * 1.5
+        print("This amount of paint can cover an area of \(areaCanCover)")
+    }
+}
+
+print(bucketsOfPaint)
+
+bucketsOfPaint = 8
