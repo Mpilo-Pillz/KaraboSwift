43f0bd0bbd4fca73af54946bb7ea0821b76f49cc
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-11-05 15:19:25 +0200

    learning setters

--- DesignPatterns/Solid/AngellaYuDeepDives/ComputedProperties.playground/Contents.swift
+++ DesignPatterns/Solid/AngellaYuDeepDives/ComputedProperties.playground/Contents.swift
@@ -5,6 +5,8 @@ import Foundation
  * Stored Slices
  */
 let pizzaInInches: Int = 12
+var numberOfPerople: Int = 6
+let slicesPerPerson: Int = 5
 
 var storedNumberOfSlices: Int = 6
 
@@ -12,9 +14,40 @@ var storedNumberOfSlices: Int = 6
 /**
  * this is a getter
  * if you have a function that does not return and out put nor take inputs consider making it a computed property or a getter
+ * and all it does it excecut a block of code
  */
 var numberOfSilces: Int {
     return pizzaInInches - 4
 }
 
+var numberOfPizza: Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSilces / slicesPerPerson
+        return numberOfPerople / numberOfPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSilces * newValue // rember newValue is bult in and is given to us for free when we use a stetter
+        numberOfPerople = totalSlices / slicesPerPerson
+    }
+}
+print(numberOfPizza)
+
+var getNumOfSlices: Int {
+    get {
+        return pizzaInInches - 4
+    }
+    set {
+        /**
+         * runs whenever this property gets a new value
+         */
+        print("Number of slices now has a new alue of: \(newValue)")
+    }
+}
+
+numberOfPizza = 4 // triggers newValue
+print(numberOfPerople)
 print(numberOfSilces)
+getNumOfSlices = 12 // automatically triggers newValue
+
+
+
