501517e03a8cb3e7e87176703f421d802252e427
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-11-05 15:29:44 +0200

    leanring observers

--- DesignPatterns/Solid/AngellaYuDeepDives/ComputedProperties.playground/Contents.swift
+++ DesignPatterns/Solid/AngellaYuDeepDives/ComputedProperties.playground/Contents.swift
@@ -4,7 +4,30 @@ import Foundation
 /**
  * Stored Slices
  */
-let pizzaInInches: Int = 12
+//let pizzaInInches: Int = 12
+var pizzaInInches: Int = 10 {
+    willSet {
+        print("the old value is \(pizzaInInches)")
+    // runs before the value changes
+        print(newValue) // built in
+        
+        
+    }
+    didSet {
+        // runs after the value changes
+        print(oldValue) // built in
+        print("the new value is \(pizzaInInches)")
+        
+        if pizzaInInches >= 18 {
+            print("Invalid size specified pizza in inches set to 18")
+            pizzaInInches = 18
+        }
+    }
+}
+
+pizzaInInches = 15
+pizzaInInches = 33 // prints 18 as 33 is invalid
+
 var numberOfPerople: Int = 6
 let slicesPerPerson: Int = 5
 
@@ -48,6 +71,14 @@ numberOfPizza = 4 // triggers newValue
 print(numberOfPerople)
 print(numberOfSilces)
 getNumOfSlices = 12 // automatically triggers newValue
+
+
+/**
+ * Observed Properties
+ * If you didnt need to compute the value with a getter
+ * but wanted to monitor the value and excecute a bock of code when the property is changed
+ * use observed properties
+ */
 
 
 
